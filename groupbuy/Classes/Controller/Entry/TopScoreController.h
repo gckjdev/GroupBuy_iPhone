1c555a29ad777cddd06b721ff558b91189dd8f9d
Benson Peng <penglingzhe@gmail.com>
2011-09-09 23:42:18 +0800

    add top score controller

---
+++ groupbuy/Classes/Controller/Entry/TopScoreController.h
@@ -0,0 +1,16 @@
+//
+//  TopScoreController.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-9.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TopScoreController : UIViewController {
+    
+}
+
+@end
