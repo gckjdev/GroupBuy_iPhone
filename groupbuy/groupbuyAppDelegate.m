b654ba3508d15218568798099c7c63c0de50706d
Benson Peng <penglingzhe@gmail.com>
2011-11-18 12:31:31 +0800

    update for product cell new implementation

--- groupbuy/groupbuyAppDelegate.m
+++ groupbuy/groupbuyAppDelegate.m
@@ -61,12 +61,17 @@ NSString* GlobalGetServerURL()
 {
 //   return @"http://192.168.1.101:8000/api/i?";
 
-   return @"http://192.168.1.188:8000/api/i?";
-//    return @"http://uhz001030.chinaw3.com/api/i?";
+//   return @"http://192.168.1.188:8000/api/i?";
+    return @"http://uhz001030.chinaw3.com/api/i?";
     
 
 //    return @"http://www.dipan100.com:8000/api/i?";
 
+}
+
+int GlobalGetProductDisplayType()
+{
+    return PRODUCT_DISPLAY_GROUPBUY;
 }
 
 CategoryService *GlobalGetCategoryService()
