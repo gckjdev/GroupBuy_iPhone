c7614fda05a27c652e614a16a53d34a405bb5771
haodong <qiuhaodong2006@126.com>
2012-01-15 17:19:38 +0800

    add DistanceController

---
+++ groupbuy/Classes/Controller/Entry/DistanceController.h
@@ -0,0 +1,13 @@
+//
+//  DistanceController.h
+//  groupbuy
+//
+//  Created by haodong qiu on 12年1月15日.
+//  Copyright (c) 2012年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DistanceController : UIViewController
+
+@end
---
+++ groupbuy/Classes/Controller/Entry/DistanceController.m
@@ -0,0 +1,51 @@
+//
+//  DistanceController.m
+//  groupbuy
+//
+//  Created by haodong qiu on 12年1月15日.
+//  Copyright (c) 2012年 orange. All rights reserved.
+//
+
+#import "DistanceController.h"
+
+@implementation DistanceController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
